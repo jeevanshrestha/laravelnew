6702e4d18f7380c3d3d9609dafe21487b287ece0
Jeevan Shrestha <jeevanshrestha09@gmail.com>
2024-04-02 10:40:53 +1100

    new laravel project

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    
+	public function index(){
+ return  "<h1>Welcome</h1> <p> This is homepage. </p><p><a href='/about'>About us</a></p>";
+}
+
+	public function about(){
+ return  "<h1>Welcome</h1> <p> This is about us page. </p><p><a href='/'>Back to home</a></p>";
+}
+
+}
